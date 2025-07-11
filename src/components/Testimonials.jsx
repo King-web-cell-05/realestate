cc4e912698e5ef0b73902bef6ed6af1336af6da8
King-web-05 <Kingsleydada159@gmail.com>
2025-07-11 12:56:42 +0100

    Update some file components

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { testimonialsData } from "../assets/assets";
+import { assets, testimonialsData } from "../assets/assets";
 
 const Testimonials = () => {
   return (
@@ -18,8 +18,16 @@ const Testimonials = () => {
       </p>
       <div className="flex flex-wrap justify-center  gap-8">
         {testimonialsData.map((testimonial, index) => (
-          <div key={index} className="max-w-[340px]   rounded px-8 py-12 text-center">
+          <div key={index} className="max-w-[340px]  bg-white rounded-lg shadow-lg px-8 py-12   ">
             <img className="w-20 h-20 rounded-full mx-auto mb-4 " src={testimonial.image} alt={testimonial.alt} />
+            <h2 className="text-xl text-gray-700 font-medium">{testimonial.name}</h2>
+            <p className="text-gray-500 mb-4 text-sm">{testimonial.title}</p>
+            <div className="flex justify-center  gap-1 text-red-500 mb-4">
+                {Array.from({length:testimonial.rating}, (item, index)=>(
+                    <img key={index} src= {assets.star_icon} alt="" />
+                ))}
+            </div>
+            <p className="text-gray-600">{testimonial.text}</p>
           </div>
         ))}
       </div>
